c9a0e0d5ed731b2c75d50cebbd680d4753d9fd97
OZGUR-KELES34 <dr.ozgurkeles@hotmail.com>
2025-03-13 12:20:19 +0300

    ilk commit
    denemeler

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function KareninAlani(kenaruzunlugu) {
 function CemberinCevresi(/* kodlar buraya */) {
   /* kodlar buraya */
 }
-
+console.log("keles")
 /* (Oto test yok) Yukarıdaki CemberinCevresi fonksiyonunu yarıçap = 5 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
 
 /* 	GÖREV 2:  
